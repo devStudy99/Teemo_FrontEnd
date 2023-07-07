82834424d636162fda1bbb37390839551e5b1418
devStudy99 <ksh990913@gmail.com>
2023-07-07 13:55:33 +0900

    Feat: Route 경로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+// import Header from '@components/Header/Header';
+import Footer from '@components/Footer/Footer';
 import './App.css';
+import Header1 from '@components/Header/Header1';
+import { Route, Routes } from 'react-router-dom';
+import Write from '@components/Write';
+import Detail from '@components/Detail';
+import BoardList from '@components/BoardList';
+import MyPage from '@components/MyPage';
 
 function App() {
-  return <div>계시나요?</div>;
+  return (
+    <>
+      {/* <Header /> */}
+      <Header1 />
+      <Routes>
+        <Route path="/write" element={<Write />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/boardList" element={<BoardList />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
